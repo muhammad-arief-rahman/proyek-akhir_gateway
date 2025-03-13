3571c13db1f29c758d5f33ccce84fd9d042a57e9
Muhammad Arief Rahman <arief21si@mahasiswa.pcr.ac.id>
2025-03-13 23:44:10 +0700

    Initial, create auth gateway

---
+++ index.ts
@@ -0,0 +1,36 @@
+import http from 'http'
+import httpProxy from 'http-proxy'
+
+const gatewayPort = process.env.PORT || 5000
+
+const services = {
+  '/auth': 5001
+}
+
+const proxy = httpProxy.createProxyServer({});
+
+http.createServer((req, res) => {
+  console.log(`Request: ${req.url}`)
+  
+  try {
+    let proxied = false
+    
+    Object.entries(services).forEach(([path, port]) => {
+      if (req.url?.startsWith(path)) {
+        proxied = true
+        req.url = req.url.slice(path.length)
+        proxy.web(req, res, { target: `${process.env.SERVICE_URL}:${port}` })
+      }
+    })
+  
+    if (!proxied) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('Not found')
+    }
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end('Internal server error')
+  }
+}).listen(process.env.PORT as unknown as number || 5000)
+
+console.log(`Gateway running at http://localhost:${gatewayPort}`)
